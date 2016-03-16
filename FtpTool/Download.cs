55ad7b991094d6b18a21e130c199dca884ead9f6
Shannon Barrett <sbarrett00@live.com>
2016-03-16 15:46:09 -0500

    Initial commit

---
+++ FtpTool/Download.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Configuration;
+
+namespace FtpTool
+{
+    public class Download
+    {
+        private readonly string _ftpServer;
+        private readonly string _userName;
+        private readonly string _password;
+        private readonly string _serverPath;
+        private readonly string _localPath;
+        private const int gMaxRetry = 5;
+        private static string gLocalPath = @"";
+
+
+        public Download(string server, string username, string password, string remotePath, string localPath)
+        {
+            _ftpServer = !string.IsNullOrWhiteSpace(server) ? server : ConfigurationManager.AppSettings["FtpServer"];
+            _userName = !string.IsNullOrWhiteSpace(username) ? username : ConfigurationManager.AppSettings["FtpUserName"];
+            _password = !string.IsNullOrWhiteSpace(password) ? password : ConfigurationManager.AppSettings["Password"];
+            _serverPath = !string.IsNullOrWhiteSpace(remotePath) ? remotePath : ConfigurationManager.AppSettings["ServerPath"];
+            _localPath = !string.IsNullOrWhiteSpace(localPath) ? localPath : ConfigurationManager.AppSettings["LocalPath"];
+        }
+
+        public int Execute()
+        {
+
+            Console.WriteLine("Downloading files to {0}", _localPath);
+            var fileList = new List<string>();
+            var downloadUri = _ftpServer + "/" + _serverPath + "/";
+            fileList = BuildFileList(downloadUri);
+
+            return DownloadImages(fileList);
+        }
+        private List<string> BuildFileList(string uri)
+        {
+            var fileList = new List<string>();
+            var serverUri = new UriBuilder("ftp", uri);
+            var request = (FtpWebRequest)WebRequest.Create(serverUri.ToString());
+            request.Credentials = new NetworkCredential(_userName, _password);
+            request.Method = WebRequestMethods.Ftp.ListDirectory;
+            var response = (FtpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            using (StreamReader reader = new StreamReader(responseStream))
+            {
+                var aLine = "";
+                while (true)
+                {
+                    aLine = reader.ReadLine();
+                    if (aLine != null)
+                    {
+                        if (aLine.Contains(".jpg"))
+                        {
+                            fileList.Add(uri + "/" + aLine);
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return fileList;
+        }
+
+        private int DownloadImages(List<string> fileList)
+        {
+            var count = 0;
+           
+            using (var request = new WebClient())
+            {
+                request.Credentials = new NetworkCredential(_userName, _password);
+
+                foreach (string key in fileList)
+                {
+                    var keyUri = "ftp://" + key;
+                    var destinationPath = key.Replace(_ftpServer, _localPath).Replace("/", @"\");
+
+                    var retryAttempt = 0;
+                    while (retryAttempt <= gMaxRetry)
+                    {
+                        try
+                        {
+                            var dr = request.ToString();
+                            request.DownloadFile(keyUri, destinationPath);
+                            retryAttempt = gMaxRetry + 1;
+                            count++;
+                        }
+                        catch (Exception ex)
+                        {
+                            retryAttempt++;
+                            Console.WriteLine("Failed to download file: {0} \n {1}", key, ex.Message);
+                            if (retryAttempt <= gMaxRetry)
+                            {
+                                Console.WriteLine("Retry attempt {0}/{1}", retryAttempt, gMaxRetry);
+                            }
+                            else
+                            {
+                                Console.WriteLine(
+                                    "Unable to download file after {0} attempts. Please engage eVox support.", gMaxRetry);
+                            }
+
+                        }
+                    }
+                }
+                return count;
+            }
+        }
+    }
+}
---
+++ FtpTool/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace FtpTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = "",
+                username = "",
+                password = "",
+                remotePath = "",
+                localPath = "";
+
+            if (args.Length == 0)
+            {
+                PrintHelp();
+                return;
+            }
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-server":
+                    case "-s":
+                        server = args[i + 1];
+                        break;
+                    case "-username":
+                    case "-u":
+                        username = args[i + 1];
+                        break;
+                    case "-p":
+                    case "-password":
+                        password = args[i + 1];
+                        break;
+                    case "-r":
+                    case "-remote":
+                        remotePath = args[i + 1];
+                        break;
+                    case "-l":
+                    case "-local":
+                        localPath = args[i + 1];
+                        break;
+                    case "-?":
+                    case "-help":
+                        PrintHelp();
+                        return;
+                }
+            }
+
+            var download = new Download(server, username, password, remotePath, localPath);
+
+            var downloadCount = download.Execute();
+
+            if (downloadCount > 0)
+            {
+                Console.WriteLine("Successfully downloaded {0} files.", downloadCount);
+            }
+            else
+            {
+                Console.WriteLine("No files were downloaded.");
+            }
+        }
+
+        public static void PrintHelp()
+        {
+            Console.WriteLine("FtpTool.exe will download jpgs from a particular ftp location.");
+            Console.WriteLine();
+            Console.WriteLine("Usage:" + "\t" + "FtpTool.exe [-s server] [-u username] [-p password] [-l local] [-r remote]");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            Console.WriteLine("\t-server, -s" + "\t\t" + "Ftp Server address");
+            Console.WriteLine("\t-username, -u" + "\t\t" + "Ftp Username");
+            Console.WriteLine("\t-password, -p" + "\t\t" + "Ftp Password");
+            Console.WriteLine("\t-local, -l" + "\t\t" + "Local download path");
+            Console.WriteLine("\t-remote, -r" + "\t\t" + "Remote folder path");
+        }
+    }
+}
